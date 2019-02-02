35d0bc957434339615396501846dd11a820b1086
Hrnek Bezucha <hrnek@x230.lan>
2019-02-02 12:28:10 +0000

    init

---
+++ bat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# get number from /sys/class/power_supply/BAT0/capacity
+
+charge = 95                 # example value
+max_points = 5
+point_value = 20            # full charge / max_points
+points = []
+
+full_heart = "♥"
+empty_heart = "♡"           # double-width.. *sigh*
+full_dot = "•"              # dots may be better
+empty_dot = "○"
+
+full_point = full_dot       # choose symbol hearts or dots
+empty_point = empty_dot
+
+for point in range(max_points):
+    """Prints full point if divisible by point_value
+        or empty if not."""
+    if charge // point_value:
+        points.append(full_point)
+        charge = charge - point_value
+    else:
+        points.append(empty_point)
+        break
+
+print(*points)
+
