7765a5e9da891089a1aec7ddf1dc024e89e80780
HrnekBezucha <4338913+HrnekBezucha@users.noreply.github.com>
2019-02-04 08:47:44 +0000

    file error handling

--- bat.py
+++ bat.py
@@ -8,6 +8,7 @@
 max_points = 5
 point_value = 20            # full charge / max_points
 points = []
+charge = 0                  # default value
 
 full_heart = "♥"
 empty_heart = "♡"           # double-width symbol.. *sigh*
@@ -18,9 +19,12 @@ full_point = full_dot       # choose symbol hearts or dots
 empty_point = empty_dot
 
 
-with open("/sys/class/power_supply/BAT0/capacity", 'r') as f:
-    """ Reads data from battery driver file."""
-    charge = int(f.read())
+try:
+    with open("/sys/class/power_supply/BAT0/capacity", 'r') as f:
+        """ Reads data from battery driver file."""
+        charge = int(f.read())
+except FileNotFoundError:
+    print("Battery charge not found.")
 
 
 for point in range(max_points):
@@ -29,11 +33,14 @@ for point in range(max_points):
 #   no reason to print when full charge
     if charge > 95:
         break
+    elif charge == 0:
+# when file does not exist
+        break
     elif charge // point_value:
         points.append(full_point)
         charge = charge - point_value
     else:
-        points.append(empty_point)
+        points.insert(0, empty_point)
         break
 
 print(*points)
